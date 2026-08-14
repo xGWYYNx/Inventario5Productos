b22510424bc040674cf06282794e163dc4902ae5
samuel <samuelcr11@outlook.com>
2026-08-14 16:45:13 -0500

    Creacion de endpoint para crear productos

--- src/main/java/com/edu/sena/Inventario5/inventarioController.java
+++ src/main/java/com/edu/sena/Inventario5/inventarioController.java
@@ -1,20 +1,34 @@
 package com.edu.sena.Inventario5;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.edu.sena.Inventario5.model.producto;
-import java.util.List;
 
 @RestController
 public class inventarioController {
-    private final List<producto> inventraio = List.of(
-    new producto(1L, "Papa pastusa", "Verduras", 2500.0),
-    new producto(2L, "Tomate", "Verduras", 3200.0),
-    new producto(3L, "Fresa", "Frutas", 8500.0)
-    );
-    @GetMapping
+
+    private final List<producto> inventario = new ArrayList<>(List.of(
+        new producto(1L, "Papa pastusa", "Verdura", 2500.0),
+        new producto(2L, "Tomate", "Verdura", 3200.0),
+        new producto(3L, "Fresa", "Fruta", 8500.0),
+        new producto(4L, "Leche", "Lácteo", 4500.0),
+        new producto(5L, "Arroz", "Grano", 3800.0)
+    ));
+
+    @GetMapping("/productos")
     public List<producto> listarProductos() {
-        return inventraio;
+        return inventario;
+    }
+
+    @PostMapping("/productos")
+    public producto agregarProducto(@RequestBody producto nuevoProducto) {
+        inventario.add(nuevoProducto);
+        return nuevoProducto;
     }
 }
